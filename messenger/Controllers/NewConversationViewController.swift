1669198896721fce41d888ed9662dc78360bb10f
tirbakh <tirbakh1998@gmail.com>
2021-05-15 00:28:23 +0300

    Add controllers

---
+++ messenger/Controllers/NewConversationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NewConversationViewController.swift
+//  messenger
+//
+//  Created by Лада Тирбах on 15.05.2021.
+//
+
+import UIKit
+
+class NewConversationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
